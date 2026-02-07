ce29b1f7afed414ada2950114055ad7c68120de4
byte-jlz <plaza.julzchristian@gmail.com>
2026-02-07 16:14:03 +0800

    init

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # This says: "When user visits the homepage (''), call the shop_view function"
     path('', views.home, name='home'),
 
     path('cart/', views.shop_view, name='cart'),
