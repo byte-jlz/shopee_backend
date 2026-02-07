2cb0bb5f6186bf25536ff892161940d5500d7e9d
byte-jlz <plaza.julzchristian@gmail.com>
2026-02-07 08:47:26 +0800

    init

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     # This says: "When user visits the homepage (''), call the shop_view function"
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
 
     path('cart/', views.shop_view, name='cart'),
+
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product, Order, OrderItem
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Order, OrderItem, Customer
 
 def shop_view(request):
     # 1. Fetch Products for the "Daily Discover" section
@@ -34,3 +34,33 @@ def home(request):
     
     # Render the new 'home.html' template
     return render(request, 'core/home.html', {'products': products})
+
+
+def add_to_cart(request, product_id):
+    # 1. Get the specific product user wants to buy
+    product = get_object_or_404(Product, id=product_id)
+
+    # 2. Get the "dummy" customer (For now, we just grab the first one in the DB)
+    """# NOTE: You MUST have created at least one Customer in Admin Panel for this to work! """
+    customer = Customer.objects.first() 
+
+    # 3. Get or Create a Pending Order for this customer
+    # (If they have a pending order, use it. If not, make a new one.)
+    order, created = Order.objects.get_or_create(
+        customer=customer, 
+        order_status='pending'
+    )
+
+    # 4. Check if this product is already in the cart
+    item, created = OrderItem.objects.get_or_create(
+        order=order, 
+        product=product,
+        defaults={'quantity': 0, 'price_at_purchase': product.price}
+    )
+
+    # 5. Increase Quantity and Save
+    item.quantity += 1
+    item.save()
+
+    # 6. Redirect the user to the Cart page to see their item
+    return redirect('cart')
